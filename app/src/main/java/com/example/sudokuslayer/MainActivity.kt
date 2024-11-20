ff4f0c405a6790c5575feee58768891cbff77520
Michauu035 <51509107+Michauu035@users.noreply.github.com>
2024-11-20 15:52:52 +0100

    project init

---
+++ app/src/main/java/com/example/sudokuslayer/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.sudokuslayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sudokuslayer.presentation.ui.theme.SudokuSlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SudokuSlayerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        SudokuGrid(
+                            gridValues = buildList(9) {
+                                (1..9).toList()
+                            },
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SudokuGrid(gridValues: List<Int>, modifier: Modifier = Modifier) {
+    return LazyVerticalGrid(columns = GridCells.Fixed(9)) {
+        items(gridValues) { value ->
+            Text(value.toString())
+        }
+    }
+}
